5734453018b866f49315b2ca4e4788e5e2bfb332
Darko <bazalac.darko@outlook.com>
2022-11-15 20:20:14 +0100

    Sending data to the server.

--- public/main.js
+++ public/main.js
@@ -1,10 +1,23 @@
 if ("geolocation" in navigator) {
   console.log("geolocation available");
-  navigator.geolocation.getCurrentPosition(position => {
+  navigator.geolocation.getCurrentPosition(async position => {
     const lat = position.coords.latitude;
     const lon = position.coords.longitude;
     document.getElementById("latitude").textContent = lat;
     document.getElementById("longitude").textContent = lon;
+    // console.log(position);
+
+    const data = { lat, lon };
+    const options = {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(data),
+    };
+    const response = await fetch("/api", options);
+    const json = await response.json();
+    console.log(json);
   });
 } else {
   console.log("geolocation not available");
