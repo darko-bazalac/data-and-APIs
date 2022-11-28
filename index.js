a66786fec0812e55ebc010d52e27aa51256cb2c2
Darko <bazalac.darko@outlook.com>
2022-11-28 22:25:35 +0100

    Added Database

--- index.js
+++ index.js
@@ -1,17 +1,28 @@
 const express = require("express");
+const Datastore = require("nedb");
 
 const app = express();
 app.listen(5500, () => console.log("listening at 5500"));
 app.use(express.static("public"));
 app.use(express.json({ limit: "1mb" }));
 
-app.post("/api", (request, response) => {
-  console.log("I got a request!");
-  console.log(request.body);
-  const data = request.body;
-  response.json({
-    status: "success",
-    latitude: data.lat,
-    longitude: data.lon,
+const database = new Datastore("database.db");
+database.loadDatabase();
+
+app.get("/api", (request, response) => {
+  database.find({}, (err, data) => {
+    if (err) {
+      response.end();
+      return;
+    }
+    response.send(data);
   });
+});
+
+app.post("/api", (request, response) => {
+  const data = request.body;
+  const timestamp = Date.now();
+  data.timestamp = timestamp;
+  database.insert(data);
+  response.json(data);
 });
